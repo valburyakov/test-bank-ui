764ff97100782e50e160c977ee041cb741bc7a49
Valentin Buryakov <valburyakov@gmail.com>
2019-04-26 00:30:35 +0300

    add new test case (#11)

--- src/app/shared/material.shared.module.ts
+++ src/app/shared/material.shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import {
+  MatAutocompleteModule,
   MatButtonModule,
   MatCardModule,
   MatChipsModule,
@@ -19,6 +20,7 @@ import {
     MatListModule,
     MatCardModule,
     MatChipsModule,
+    MatAutocompleteModule,
     MatProgressSpinnerModule,
     MatProgressBarModule
   ],
@@ -31,6 +33,7 @@ import {
     MatListModule,
     MatCardModule,
     MatChipsModule,
+    MatAutocompleteModule,
     MatProgressSpinnerModule,
     MatProgressBarModule
   ]
